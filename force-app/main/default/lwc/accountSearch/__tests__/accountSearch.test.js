9aebffb36e14460b0aa76152821b02d336b7a2ab
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-06-17 18:06:04 -0400

    Work with Salesforce Data (LWC for VF)
    Add in Apex controller AccountListControllerLwc as with two cacheable
    methods. Adapted accountFinder and accountSearch to use Apex over the
    wire.
    
    Also updated Jest tests to use mocks for the @wire Apex methods and test
    the mocked results against component properties and markup.

--- force-app/main/default/lwc/accountSearch/__tests__/accountSearch.test.js
+++ force-app/main/default/lwc/accountSearch/__tests__/accountSearch.test.js
@@ -1,5 +1,17 @@
 import { createElement } from 'lwc';
 import AccountSearch from 'c/accountSearch';
+import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+import queryAccountsByEmployeeNumber from '@salesforce/apex/AccountListControllerLwc.queryAccountsByEmployeeNumber';
+
+// Realistic data with a list of accounts
+const mockQueryAccountsByEmployeeNumber = require('./data/queryAccountsByEmployeeNumber.json');
+
+// An empty list of records to verify the component does something reasonable
+// when there is no data to display
+const mockQueryAccountsByEmployeeNumberNoRecords = require('./data/queryAccountsByEmployeeNumberNoRecords.json');
+
+// Register as Apex wire adapter. Some tests verify that provisioned values trigger desired behavior.
+const queryAccountsByEmployeeNumberAdapter = registerApexTestWireAdapter(queryAccountsByEmployeeNumber);
 
 describe('c-account-search', () => {
   afterEach(() => {
@@ -36,6 +48,61 @@ describe('c-account-search', () => {
     buttonElement.dispatchEvent(new CustomEvent('click'));
     return Promise.resolve().then(() => {
       expect(inputElement.value).toBe(null);
+    });
+  });
+
+  describe('queryAccountsByEmployeeNumber @wire data', () => {
+    it('renders four records', () => {
+      const element = createElement('c-account-search', {
+        is: AccountSearch
+      });
+      document.body.appendChild(element);
+
+      // Emit data from @wire
+      queryAccountsByEmployeeNumberAdapter.emit(mockQueryAccountsByEmployeeNumber);
+
+      return Promise.resolve().then(() => {
+        // Select elements for validation
+        const accountElements = element.shadowRoot.querySelectorAll('p');
+        expect(accountElements.length).toBe(mockQueryAccountsByEmployeeNumber.length);
+        expect(accountElements[0].textContent).toBe(mockQueryAccountsByEmployeeNumber[0].Name);
+      });
+    });
+
+    it('renders no items when no records are returned', () => {
+      const element = createElement('c-account-search', {
+        is: AccountSearch
+      });
+      document.body.appendChild(element);
+
+      // Emit data from @wire
+      queryAccountsByEmployeeNumberAdapter.emit(mockQueryAccountsByEmployeeNumberNoRecords);
+
+      return Promise.resolve().then(() => {
+        // Select elements for validation
+        const accountElements = element.shadowRoot.querySelectorAll('p');
+        expect(accountElements.length).toBe(
+          mockQueryAccountsByEmployeeNumberNoRecords.length
+        );
+      });
+    });
+  });
+
+  describe('queryAccountsByEmployeeNumber @wire error', () => {
+    it('shows error panel element', () => {
+      const element = createElement('c-account-search', {
+        is: AccountSearch
+      });
+      document.body.appendChild(element);
+
+      // Emit error from @wire
+      queryAccountsByEmployeeNumberAdapter.error();
+
+      return Promise.resolve().then(() => {
+        const errorElement = element.shadowRoot.querySelector('p');
+        expect(errorElement).not.toBeNull();
+        expect(errorElement.textContent).toBe('No accounts found.');
+      });
     });
   });
 });
